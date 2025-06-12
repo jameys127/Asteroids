768a84880c320a54470a9ca707c78e9358ffb8a9
Jeremiah Sheehy <jameys127@gmail.com>
2025-06-11 22:37:36 -0700

    created a main menu and imported files for TextMeshPro

---
+++ Asteroids/Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+    public void Play(){
+        SceneManager.LoadScene("GameplayScene");
+    }
+
+    public void Leaderboard(){
+        SceneManager.LoadScene("LeaderboardScene");
+    }
+
+    public void Quit(){
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+    }
+}
